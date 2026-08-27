591cca4733caac639f753a3455219b09596850ac
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-08-27 18:37:43 -0500

    Getters y setter para los atributos privados tiempo de estadía y acompaña a, de la clase Visitante

--- HerenciaPOO/src/herenciapoo/Visitante.java
+++ HerenciaPOO/src/herenciapoo/Visitante.java
@@ -15,4 +15,27 @@ public class Visitante extends Persona {
     private double tiempoEstadia;
     private String acompaniaA;
     
+    // Métodos getter y setter para la clase hija Visitante
+    
+    // Tiempo estadía
+    
+    public double getTiempoEstadia() {
+        return tiempoEstadia;
+    }
+
+    public void setTiempoEstadia(int tiempoEstadia) {
+        this.tiempoEstadia = tiempoEstadia;
+    }
+
+    // Acompaña a
+    
+    public String getAcompaniaA() {
+        return acompaniaA;
+    }
+
+    public void setAcompaniaA(String quienAcompania) {
+        this.acompaniaA = quienAcompania;
+    }
+    
+    
 }
