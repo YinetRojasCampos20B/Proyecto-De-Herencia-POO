dffa6ceb024eb6a94c4c76e935f3647de7fd4574
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-08-27 18:44:44 -0500

    Asignando valores a los atributos del objeto residente 1, en este caso, su nombre, apellido, edad, identificador del residente en el sistema, su hora de entrada, salida y año de mudanza

--- HerenciaPOO/src/herenciapoo/HerenciaPOO.java
+++ HerenciaPOO/src/herenciapoo/HerenciaPOO.java
@@ -18,6 +18,18 @@ public class HerenciaPOO {
         // Instanciando un objeto Residente 1 de clase hija Residente
 
         Residente residente1 = new Residente();
+    
+        // Asignando atributos al objeto Residente 1
+       
+        residente1.setNombres("Maria Antonieta");
+        residente1.setApellidos(" de Francia");
+        residente1.setEdad(41);
+        residente1.setId(1);
+        residente1.setHoraEntrada(17.20f);
+        residente1.setHoraSalida(20.40f);
+        residente1.setAñoMudanza(2021);
+    
+    
     }
     
 }
