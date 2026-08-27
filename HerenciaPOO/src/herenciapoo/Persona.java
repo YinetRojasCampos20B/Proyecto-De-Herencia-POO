46fec6ab4d40ed96748b52f50ef9abe59cb3c76f
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-08-27 18:03:44 -0500

    Construyendo los métodos getter y setter para cada uno de los atributos definidos en la superclase Persona, en este caso, el id, la hora de entrada y de salida

--- HerenciaPOO/src/herenciapoo/Persona.java
+++ HerenciaPOO/src/herenciapoo/Persona.java
@@ -20,4 +20,65 @@ public class Persona {
     private float horaEntrada;
     private float horaSalida;
     
+    // Métodos getter y setter para la clase Persona
+    // Nombres
+    
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombre) {
+        this.nombres = nombre;
+    }
+
+    // Apellidos
+    
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    // Edad
+    
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    // Identificador
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Hora de entrada
+    
+    public float getHoraEntrada() {
+        return horaEntrada;
+    }
+
+    public void setHoraEntrada(float horaEntrada) {
+        this.horaEntrada = horaEntrada;
+    }
+
+    // Hora de salida
+    
+    public float getHoraSalida() {
+        return horaSalida;
+    }
+
+    public void setHoraSalida(float horaSalida) {
+        this.horaSalida = horaSalida;
+    }
+    
 }
